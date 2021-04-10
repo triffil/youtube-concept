f4470f9f160135f03471aa9043c5741d4e704057
triffil <72388044+triffil@users.noreply.github.com>
2021-04-10 22:58:09 +0300

    Зафиксировали меню и шапку, пофиксили их и добавили контент
    Сделали в контенте слайдер и 2 заготовки для дальнейших слайдов

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".channel-button-next",
+    prevEl: ".channel-button-prev",
+  },
+});
