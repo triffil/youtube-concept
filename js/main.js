196f3046112e4bc217e75ad65c42e96bbf23a8d4
triffil <72388044+triffil@users.noreply.github.com>
2021-04-11 15:58:11 +0300

    Доделали 2 оставшихся слайдера

--- js/main.js
+++ js/main.js
@@ -9,3 +9,27 @@ const swiper = new Swiper(".swiper-container", {
     prevEl: ".channel-button-prev",
   },
 });
+
+const swiperRecommended = new Swiper(".recommended-slider", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".recommended-button-next",
+    prevEl: ".recommended-button-prev",
+  },
+});
+
+const swiperChannelForYouSlider = new Swiper(".channel-for-you-slider", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".channel-for-you-button-prev",
+    prevEl: ".channel-for-you-button-next",
+  },
+});
