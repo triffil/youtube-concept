e42ad78ee1dc4924c8bff680b1ff0fa6f3f26d53
triffil <72388044+triffil@users.noreply.github.com>
2021-04-22 12:32:03 +0300

    Добавили правила для адаптива

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
-const swiper = new Swiper(".swiper-container", {
+const swiper = new Swiper(".channel-slider", {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
 
+    1600: {
+      slidesPerView: 5,
+    },
+    1330: {
+      slidesPerView: 4,
+    },
+    1090: {
+      slidesPerView: 3,
+    },
+    825: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: ".channel-button-next",
@@ -13,8 +31,16 @@ const swiper = new Swiper(".swiper-container", {
 const swiperRecommended = new Swiper(".recommended-slider", {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
-
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1350: {
+      slidesPerView: 3,
+    },
+    900: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: ".recommended-button-next",
@@ -25,11 +51,41 @@ const swiperRecommended = new Swiper(".recommended-slider", {
 const swiperChannelForYouSlider = new Swiper(".channel-for-you-slider", {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
 
+    1600: {
+      slidesPerView: 5,
+    },
+    1330: {
+      slidesPerView: 4,
+    },
+    1090: {
+      slidesPerView: 3,
+    },
+    825: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: ".channel-for-you-button-prev",
     prevEl: ".channel-for-you-button-next",
   },
 });
+
+const mobileSearch = document.querySelector(".mobile-search");
+const inputGroup = document.querySelector(".input-group");
+mobileSearch.addEventListener("click", () => {
+  inputGroup.classList.toggle("is-open");
+});
+
+if (document.documentElement.scrollWidth <= 624) {
+  swiper.destroy();
+  swiperRecommended.destroy();
+  swiperChannelForYouSlider.destroy();
+}
